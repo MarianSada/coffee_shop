3fe3882e26219e8e11033ffad568388d313acac4
MarianSada <marian.sada@gmail.com>
2023-06-15 13:40:51 +0300

    webkit

--- src/components/Section.styles.js
+++ src/components/Section.styles.js
@@ -4,11 +4,16 @@ import { maxDevices } from './breakpoints/Breakpoints'
 export const StyledSection = styled.section`
   position: relative;
   display: flex;
+  -webkit-display: flex;
   align-items: center;
+  -webkit-align-items: center;
   text-align: center;
   flex-wrap: wrap;
+  -webkit-flex-wrap: wrap;
   justify-content: space-between;
+  -webkit-justify-content: space-between;
   gap: 20px;
+  -webkit-gap: 20px;
   padding: 100px;
   @media only screen and (${maxDevices.lg}) {
     padding: 50px 20px;
